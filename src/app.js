3ebd16810229900c94601c6d0ecee94c49bb4d20
RafaAugustoFS <rafaelaugustofsilva@gmail.com>
2024-10-03 16:37:24 -0300

    continuando...

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 require(dotenv).config();
-const express = require('express');
+const express = require("express");
+const { sequelize } = require("./models");
 
 const app = express();
 app.use(express.json());
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("Conexão com o banco de dados com êxito!");
+  })
+  .catch((err) => {
+    console.error("Erro ao conecar ao banco: ", err);
+  });
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log("---------------------------");
-    console.log(`Running on http://${PORT}`);
-    console.log("---------------------------");
-})
+app.listen(PORT, () => {
+  console.log("---------------------------");
+  console.log(`Running on http://${PORT}`);
+  console.log("---------------------------");
+});
---
+++ src/router/userRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.post('/', );
+router.get('/', );
+router.put('/:id', );
+router.delete('/:id', );
+router.get(':id', );
+
+module.exports = router;
